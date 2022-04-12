04212b287959c8afa2e42e1447be040ae92bd477
timurlik <timurbibnev@gmail.com>
2022-04-12 11:21:01 +0300

    Completed tasks from manual

--- src/LessonTwo.java
+++ src/LessonTwo.java
@@ -1,66 +1,53 @@
 public class LessonTwo {
-    public static void main (String[]args) {
-        doAnotherNestedLoop();
-
-        /*char operator = '*';
-        switch (operator) {
-            case '+':
-                System.out.println(sum(2,3));
-                break;
-            case '-':
-                System.out.println(minus(2,3));
-                break;
-            case '*':
-                System.out.println(multiply(2,3));
-                break;
-            case '/':
-                System.out.println(divide(2,3));
-                break;
-        }*/
+    public static void main(String[] args) {
+    fieldFour();
 
     }
-    private static int sum (int a, int b) {
-        return a + b;
-    }
-    private static int minus ( int a, int b) {
-        return a - b;
-    }
-
-    private static int multiply (int a, int b) {
-        return a * b;
-    }
-    private static int divide (int a, int b) {
-        return a / b;
-    }
-    private static void printNum() {
-        for(int i = 1000000; i > 0; i = i -2){
-            System.out.println(i);
-        }
-        for (int i = 0; i < 0; i += 2) {
-            System.out.println(i);
-        }
-    }
-    private static void doNestedLoop() {
-        for (int c = 1; c < 4; c++) {
-            for (int j = 1; j < 4; j++) {
-                System.out.println("c = " + c + "; j = " + j);
+    public static void fieldOne() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (j < 3) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("0 ");
+                }
             }
+            System.out.println();
         }
     }
-    private static void doWhileMethod() {
-        int x = 3;
-        while (x < 100000) {
-            x = (x + 4) * x + 12;
-            System.out.println(x);
+    public static void fieldTwo() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 0 || i == 2 || j == 0 || j == 2 ) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print("* ");
+                }
+            }  System.out.println();
         }
     }
-    private static void doAnotherNestedLoop() {
-        int c = 0;
-        for (int i = 1; i < 5; i ++) {
-            for ( int j = 1; j < 5; j++) {
-                c = c + i - j;
-                System.out.println(c);
-            }
+    public static void fieldThree() {
+       for (int i = 0; i < 6; i++) {
+           for (int j = 0; j < 6; j++) {
+               if ((i + j) % 2 == 0) {
+                   System.out.print("* ");
+               }    else {
+                   System.out.print("0 ");
+               }
+           }    System.out.println();
+       }
+    }
+    public static void fieldFour() {
+        for (int i = 0; i < 6; i ++) {
+            for (int j = 0; j < 6; j++) {
+                if (j > i) {
+                    System.out.print("* ");
+                }   else if (i > j) {
+                    System.out.print("0 ");
+                }   else {
+                    System.out.print(" ");
+                }
+            }   System.out.println();
         }
     }
 }
